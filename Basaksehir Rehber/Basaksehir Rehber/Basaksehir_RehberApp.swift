382358aba55e778ddf1cdaf189aba5c29b5a5154
Semih Kesgin <SemihK@users.noreply.github.com>
2023-07-28 13:01:14 +0300

    create

---
+++ Basaksehir Rehber/Basaksehir Rehber/Basaksehir_RehberApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basaksehir_RehberApp.swift
+//  Basaksehir Rehber
+//
+//  Created by Semih Kesgin on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Basaksehir_RehberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
